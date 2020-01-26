634d55bca24943901650eb72b42fc4ce092f5ca8
Xiao Liang <liangxi@liangxi-a02.vmware.com>
2020-01-26 11:34:53 +0800

    view.py

--- challenge/challenge/view.py
+++ challenge/challenge/view.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def hello(request):
-    return HttpResponse("Hello world ! ")
+def get_result(request):
+    context = {}
+    context['res_str'] = 'Hello World!'
+    return render(request, 'page.html', context)
